b1e1c3e52d0a5dfadf43bc3b30f11cab3370589f
reganputra <ramadhanregan334@gmail.com>
2025-01-30 08:01:04 +0700

    feat: get user data by token

---
+++ controllers/UserController.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+const prisma = require('../prisma/client/client');
+
+
+const findUsers = async (req, res) => {
+    try {
+
+        //get all users from database
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            },
+            orderBy: {
+                id: "desc",
+            },
+        });
+
+        //send response
+        res.status(200).send({
+            success: true,
+            message: "Get all users successfully",
+            data: users,
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+module.exports = { findUsers };
--- routes/router.js
+++ routes/router.js
@@ -1,17 +1,21 @@
-
-const express = require('express')
-
+const express = require('express');
 const router = express.Router();
 
-//import validate register
-const {registerValidation, loginValidation} = require('../utils/authHelper');
+// Import validation functions
+const { registerValidation, loginValidation } = require('../utils/authHelper');
 
-// register and login controller
+// Import verify token middleware
+const {verifyToken}  = require('../middleware/auth');
+
+// Import controllers
 const registerController = require('../controllers/RegisterController');
 const loginController = require('../controllers/LoginController');
+const userController = require('../controllers/UserController');
 
-//  define routes
+
+// Define routes
 router.post('/register', registerValidation, registerController.userRegister);
 router.post('/login', loginValidation, loginController.userLogin);
+router.get('/admin/users',verifyToken, userController.findUsers);
 
 module.exports = router;
