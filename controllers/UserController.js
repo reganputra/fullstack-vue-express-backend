96ed8c605b968c9d27248a2465f3468259fb91e1
reganputra <ramadhanregan334@gmail.com>
2025-01-30 08:12:05 +0700

    feat: inder user data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const prisma = require('../prisma/client/client');
+const {validationResult} = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 
 const findUsers = async (req, res) => {
@@ -33,4 +35,42 @@ const findUsers = async (req, res) => {
     }
 };
 
-module.exports = { findUsers };
+ const createUser = async (req, res) => {
+
+     const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                success: false,
+                message: "Validation error",
+                errors: errors.array(),
+            });
+        }
+
+     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        try{
+        //     insert user data
+            const user = await prisma.user.create({
+                data: {
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hashedPassword,
+                },
+            });
+
+            res.status(200).send({
+                success: true,
+                message: "User created successfully",
+                data: user,
+            });
+
+        }catch (error){
+            res.status(500).send({
+                success: false,
+                message: "Internal server error",
+            });
+        }
+ }
+
+module.exports = { findUsers, createUser };
--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // Import validation functions
 const { registerValidation, loginValidation } = require('../utils/authHelper');
+const {userValidation} = require("../utils/userHelper");
 
 // Import verify token middleware
 const {verifyToken}  = require('../middleware/auth');
@@ -13,9 +14,11 @@ const loginController = require('../controllers/LoginController');
 const userController = require('../controllers/UserController');
 
 
+
 // Define routes
 router.post('/register', registerValidation, registerController.userRegister);
 router.post('/login', loginValidation, loginController.userLogin);
 router.get('/admin/users',verifyToken, userController.findUsers);
+router.post('/admin/users',verifyToken, userValidation ,userController.createUser);
 
 module.exports = router;
