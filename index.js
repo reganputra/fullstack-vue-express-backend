e5bfeff0efacabd85c71dd7bf7a3a4a1b65fe166
reganputra <ramadhanregan334@gmail.com>
2025-05-20 17:21:08 +0700

    cors remove2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const router = require('./routes/router');
 
 // use cors
-app.use(cors('https://fullstack-vue-express-frontend.vercel.app/'));
+// app.use(cors('https://fullstack-vue-express-frontend.vercel.app/'));
 
 // define port
 const port = 3000;
